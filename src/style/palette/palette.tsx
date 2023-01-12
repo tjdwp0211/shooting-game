3ef51843eb650529cb34d6bc523ff34b779a1a35
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-12 15:42:22 +0900

    style: color
    - add palette(colors) file in style folder

---
+++ src/style/palette/palette.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const { white, beige, blue, gray, black } = {
+  white: "#F2F2F2",
+  beige: "#D9CDB8",
+  blue: "#3279A6",
+  gray: "#3B3C40",
+  black: "#00010D",
+};
+
+export { white, beige, blue, gray, black };
