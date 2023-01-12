14d7b5e780a5083c33ccbd5cc1b532a06b9a3dfc
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-12 20:21:40 +0900

    style: apply
    - apply globalStyle in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { globalStyle } from "../style/global";
+import { Global } from "@emotion/react";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
