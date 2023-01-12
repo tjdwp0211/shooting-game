f002ef0f22cbec70628ae23555bac353e928a8c6
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-12 13:53:01 +0900

    build: next.config
    - add fontLoaders key in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  experimental: {
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: { subsets: ["latin", "korean"] },
+      },
+    ],
+  },
 };
 
 export default nextConfig;
