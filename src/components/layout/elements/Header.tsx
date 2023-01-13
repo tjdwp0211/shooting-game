28a58edceb0234c094bdf4500403b29933943d92
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-13 18:32:00 +0900

    style: layout
    - create Header
    
    Fixs #3

---
+++ src/components/layout/elements/Header.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { boldFont } from "../../../style/fonts/inedx";
+import { black, white } from "../../../style/palette/palette";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <TextButton>What About Your Click</TextButton>
+      <TextButton>Board</TextButton>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.header`
+  width: 100vw;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 5vw;
+  color: ${black};
+  &,
+  * {
+    background-color: ${white};
+  }
+`;
+
+const TextButton = styled.button`
+  border: none;
+  font-size: 20px;
+  ${boldFont}
+`;
