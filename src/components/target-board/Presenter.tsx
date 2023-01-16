2fad1476f150d0623d87750cf3b397210229eb51
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-16 19:20:32 +0900

    style
    - add initial animation code in TargetBoard/Presenter

--- src/components/target-board/Presenter.tsx
+++ src/components/target-board/Presenter.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import styled from "@emotion/styled";
 import Circle from "./elements/Circle";
 
-function TargetBoard() {
+interface PresenterProps {
+  handleClicked: () => void;
+}
+
+function Presenter({ handleClicked }: PresenterProps) {
   return (
-    <Wrapper size={"10vw"}>
+    <Wrapper size={"10vw"} onClick={handleClicked}>
       <Circle radius={100}></Circle>
       <Circle radius={70}></Circle>
       <Circle radius={40}></Circle>
@@ -12,25 +16,27 @@ function TargetBoard() {
   );
 }
 
-export default TargetBoard;
+export default Presenter;
 
-const Wrapper = styled.div<{ size: string }>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const Wrapper = styled.div<{
+  size: string;
+}>`
   width: ${(props) => props.size};
   max-width: 100px;
   min-width: 52px;
   height: ${(props) => props.size};
   max-height: 100px;
   min-height: 52px;
-  position: absolute;
-  top: 100px;
-  left: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   animation: popout 0.3s ease;
   @keyframes popout {
+    from {
+      transform: scale(0);
+    }
     80% {
-      transform: scale(1.1);
+      transform: scale(1.2);
     }
     to {
       transform: scale(1);
