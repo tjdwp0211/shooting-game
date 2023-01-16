a2f1b2108de1f6177c7c41c6eff941b06a2ab6c4
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-16 20:23:06 +0900

    style: Presenter
    - add Line Component in TargetBoard Presenter
    
    Close #3

--- src/components/target-board/Presenter.tsx
+++ src/components/target-board/Presenter.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styled from "@emotion/styled";
 import Circle from "./elements/Circle";
+import Line from "./elements/Line";
+import Wrapper from "./elements/Wrapper";
 
 interface PresenterProps {
   handleClicked: () => void;
@@ -8,38 +9,14 @@ interface PresenterProps {
 
 function Presenter({ handleClicked }: PresenterProps) {
   return (
-    <Wrapper size={"10vw"} onClick={handleClicked}>
+    <Wrapper handleClicked={handleClicked}>
       <Circle radius={100}></Circle>
       <Circle radius={70}></Circle>
       <Circle radius={40}></Circle>
+      <Line rotation={true} />
+      <Line rotation={false} />
     </Wrapper>
   );
 }
 
 export default Presenter;
-
-const Wrapper = styled.div<{
-  size: string;
-}>`
-  width: ${(props) => props.size};
-  max-width: 100px;
-  min-width: 52px;
-  height: ${(props) => props.size};
-  max-height: 100px;
-  min-height: 52px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  animation: popout 0.3s ease;
-  @keyframes popout {
-    from {
-      transform: scale(0);
-    }
-    80% {
-      transform: scale(1.2);
-    }
-    to {
-      transform: scale(1);
-    }
-  }
-`;
