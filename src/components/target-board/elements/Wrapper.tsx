8e9649ddbf37cee42a6a3082828ab07651ac2fd0
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-16 20:24:48 +0900

    feat: animation
    - add animation code in Wrapper
    - animation : slip-away-motion, come-out-motion
    
    Fixs #4

--- src/components/target-board/elements/Wrapper.tsx
+++ src/components/target-board/elements/Wrapper.tsx
@@ -1,7 +1,54 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
-function Wrapper() {
-  return <></>;
+interface WrapperProps {
+  children: React.ReactNode;
+  handleClicked: () => void;
+}
+
+function Wrapper({ children, handleClicked }: WrapperProps) {
+  return <Container onClick={handleClicked}>{children}</Container>;
 }
 
 export default Wrapper;
+
+const Container = styled.div`
+  width: 10vw;
+  max-width: 100px;
+  min-width: 52px;
+  height: 10vw;
+  max-height: 100px;
+  min-height: 52px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: come-out-motion 0.2s ease;
+  :active {
+    animation: slip-away-motion 0.2s ease;
+  }
+
+  @keyframes come-out-motion {
+    from {
+      transform: scale(0);
+    }
+    80% {
+      transform: scale(1.2);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes slip-away-motion {
+    from {
+      transform: scale(1);
+    }
+    80% {
+      transform: scale(0.5);
+    }
+    to {
+      transform: scale(0);
+    }
+  }
+`;
