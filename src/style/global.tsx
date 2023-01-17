991b6d0097046b5cb1312a7f54dd4111bb79eeaa
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-17 15:35:03 +0900

    style: dark mode
    - dark mode styling in global
    - text button hover styling in global
    
    #5

--- src/style/global.tsx
+++ src/style/global.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { css } from "@emotion/react";
 import { regularFont } from "./fonts/inedx";
-import { blue, white } from "./palette/palette";
+import { black, blue, white } from "./palette/palette";
 
 const globalStyle = css`
   * {
@@ -9,13 +9,33 @@ const globalStyle = css`
   }
   body {
     margin: 0px;
-    color: ${white};
     background-color: ${blue};
+    color: ${white};
+    @media (prefers-color-scheme: dark) {
+      background-color: ${black};
+      color: ${white};
+    }
     ${regularFont}
   }
   a,
   button {
     cursor: pointer;
+    width: fit-content;
+    height: fit-content;
+  }
+  .text-button:hover {
+    animation: button-hover-motion 0.2s ease;
+    @keyframes button-hover-motion {
+      from {
+        transform: scale(0.95);
+      }
+      50% {
+        transform: scale(1.05);
+      }
+      to {
+        transform: scale(1);
+      }
+    }
   }
   a {
     text-decoration: none;
