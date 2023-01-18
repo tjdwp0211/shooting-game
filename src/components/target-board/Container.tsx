bbe943368995134e758b1e59857fac0eb08ead6e
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-18 16:15:54 +0900

    feat: target board #12, #11
    - Apply about type refactoring #11
    - Game start when user click target board #12

--- src/components/target-board/Container.tsx
+++ src/components/target-board/Container.tsx
@@ -1,14 +1,8 @@
 import React, { useCallback, useState } from "react";
-import styled from "@emotion/styled";
 import Presenter from "./Presenter";
+import { ContainerProps, TargetInfo } from "../../type/targetBoardType";
 
-interface TargetInfo {
-  x: number;
-  y: number;
-  hit: boolean;
-}
-
-function Container() {
+function Container({ gameStart, handleGameStart }: ContainerProps) {
   const [targetInfo, setTargetInfo] = useState<TargetInfo>({
     x: Math.random(),
     y: Math.random(),
@@ -22,6 +16,7 @@ function Container() {
     }),
     []
   );
+
   const handleHit = () => {
     setTargetInfo((prev) => ({
       ...prev,
@@ -29,37 +24,16 @@ function Container() {
       ...randomCoordinates(),
     }));
   };
+  console.log(gameStart);
 
   return (
-    <Changer x={targetInfo.x} y={targetInfo.y}>
-      <Presenter handleHit={handleHit} />
-    </Changer>
+    <Presenter
+      gameStart={gameStart}
+      targetInfo={targetInfo}
+      handleGameStart={handleGameStart}
+      handleHit={handleHit}
+    />
   );
 }
 
 export default Container;
-
-const Changer = styled.div<{
-  x: number;
-  y: number;
-}>`
-  width: fit-content;
-  height: fit-content;
-  position: absolute;
-  @media (width > 1020px) {
-    transform: translateX(${(props) => props.x * 1000}px)
-      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
-  }
-  @media (830px < width < 1020px) {
-    transform: translateX(${(props) => props.x * 700}px)
-      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
-  }
-  @media (500px < width < 830px) {
-    transform: translateX(${(props) => props.x * 500}px)
-      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
-  }
-  @media (width < 500px) {
-    transform: translateX(${(props) => props.x * 300}px)
-      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
-  }
-`;
--- src/components/target-board/Presenter.tsx
+++ src/components/target-board/Presenter.tsx
@@ -1,20 +1,68 @@
 import React from "react";
+import styled from "@emotion/styled";
 import { Circle, Line, Wrapper } from "./elements/";
+import { PresenterProps } from "../../type/targetBoardType";
 
-interface PresenterProps {
-  handleHit: () => void;
-}
+function Presenter(props: PresenterProps) {
+  const { targetInfo, gameStart, handleHit, handleGameStart } = props;
 
-function Presenter({ handleHit }: PresenterProps) {
+  const waitingForGameStart = (children: React.ReactNode) => {
+    return !gameStart ? (
+      <CoordinatesFixer onClick={handleGameStart}>{children}</CoordinatesFixer>
+    ) : (
+      <CoordinatesChanger x={targetInfo.x} y={targetInfo.y}>
+        {children}
+      </CoordinatesChanger>
+    );
+  };
+
   return (
-    <Wrapper handleHit={handleHit}>
-      <Circle radius={100}></Circle>
-      <Circle radius={70}></Circle>
-      <Circle radius={40}></Circle>
-      <Line rotation={true} />
-      <Line rotation={false} />
-    </Wrapper>
+    <>
+      {waitingForGameStart(
+        <Wrapper handleHit={handleHit}>
+          <Circle radius={100}></Circle>
+          <Circle radius={70}></Circle>
+          <Circle radius={40}></Circle>
+          <Line rotation={true} />
+          <Line rotation={false} />
+        </Wrapper>
+      )}
+    </>
   );
 }
 
 export default Presenter;
+
+const Root = styled.div`
+  width: fit-content;
+  height: fit-content;
+  position: absolute;
+`;
+
+const CoordinatesChanger = styled(Root)<{
+  x: number;
+  y: number;
+}>`
+  @media (width > 1020px) {
+    transform: translateX(${(props) => props.x * 1000}px)
+      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
+  }
+  @media (830px < width < 1020px) {
+    transform: translateX(${(props) => props.x * 700}px)
+      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
+  }
+  @media (500px < width < 830px) {
+    transform: translateX(${(props) => props.x * 500}px)
+      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
+  }
+  @media (width < 500px) {
+    transform: translateX(${(props) => props.x * 300}px)
+      translateY(${(porps) => porps.y * 200}px) scaleX(1) scaleY(1);
+  }
+`;
+
+const CoordinatesFixer = styled(Root)`
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
