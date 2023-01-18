461e3110d3f8265971be9869b70772e0bcd70333
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-18 18:11:49 +0900

    style: new color #14
    - Add new color in palette

--- src/style/palette/palette.tsx
+++ src/style/palette/palette.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const { white, beige, blue, gray, black } = {
+const { white, beige, blue, gray, lightGray, black } = {
   white: "#F2F2F2",
   beige: "#D9CDB8",
   blue: "#3279A6",
   gray: "#3B3C40",
+  lightGray: "#ffffffb2",
   black: "#00010D",
 };
 
-export { white, beige, blue, gray, black };
+export { white, beige, blue, gray, lightGray, black };
