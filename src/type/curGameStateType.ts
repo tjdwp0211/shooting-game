030453af2f8f57cb4f77cb110680711305c74f39
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-18 16:08:15 +0900

    refactor: type #11
    - Add type file for CurGameState Component

---
+++ src/type/curGameStateType.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface CurGameStateProps {
+  gameStart: boolean;
+}
+
+interface TimerProps {
+  gameStart: boolean;
+}
+
+export type { CurGameStateProps, TimerProps };
