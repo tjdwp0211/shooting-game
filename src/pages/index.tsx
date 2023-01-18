71aa180d811897c93b466873d096bd2783b06fd4
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-18 18:18:46 +0900

    style: UI #12
    - Add Texts UI before game start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import { Layout, Box, TargetBoard, CurGameState } from "../components";
+import { boldFont, regularFont } from "../style/fonts/inedx";
+import { TargetBoard, CurGameState, Layout, Box, Text } from "../components";
 
 function Home() {
   const [gameStart, setGameStart] = useState(false);
   const handleGameStart = () => setGameStart((prev) => !prev);
 
+  const containTexts = () => {
+    return (
+      <TextsWrapper>
+        <Text size={40} weight={boldFont}>
+          Accurate Aim, Fast Hit
+        </Text>
+        <Text size={20} weight={regularFont}>
+          Click the target to game start
+        </Text>
+      </TextsWrapper>
+    );
+  };
+
   return (
     <Layout indexPage>
       <Wrapper>
+        {!gameStart && containTexts()}
         <TargetBoard gameStart={gameStart} handleGameStart={handleGameStart} />
       </Wrapper>
       <CurGameState gameStart={gameStart} />
       <BoxWrapper>
-        <Box></Box>
+        <Box>
+          <></>
+        </Box>
       </BoxWrapper>
     </Layout>
   );
@@ -24,6 +41,18 @@ export default Home;
 const Wrapper = styled.div`
   width: 100%;
   height: 500px;
+`;
+
+const TextsWrapper = styled.div`
+  text-align: center;
+  height: 90%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 12px;
+  p {
+    margin: 0px;
+  }
 `;
 
 const BoxWrapper = styled.div`
