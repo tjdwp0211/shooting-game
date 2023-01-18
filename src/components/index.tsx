16e21a4c90208f9b8c035a2058e2f583bd97235d
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-18 17:42:31 +0900

    refactor
    - Text Component export in components/index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import Container from "./target-board/Container";
+import CurGameState from "./cur-game-info/CurGameState";
 import Layout from "./layout/Layout";
 import Box from "./box/Box";
-import CurGameState from "./cur-game-info/CurGameState";
+import Text from "./text/Text";
 
-export { Container as TargetBoard, CurGameState, Layout, Box };
+export { Container as TargetBoard, CurGameState, Layout, Box, Text };
