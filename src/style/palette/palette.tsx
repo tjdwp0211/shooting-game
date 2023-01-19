feb920880a3933ad88353b80e421a5fceec994e8
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-19 15:07:52 +0900

    style: palette #16
    - Add new colors

--- src/style/palette/palette.tsx
+++ src/style/palette/palette.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 
-const { white, beige, blue, gray, lightGray, black } = {
-  white: "#F2F2F2",
+const {
+  white,
+  beige,
+  red,
+  yellow,
+  blue,
+  lightBlue,
+  gray,
+  lightGray,
+  black,
+  lightBlack,
+} = {
+  white: "#f2f5f5",
   beige: "#D9CDB8",
-  blue: "#3279A6",
+  red: "#fc1212",
+  yellow: "#edea26",
+  blue: "#1a21ed",
+  lightBlue: "#3279A6",
   gray: "#3B3C40",
   lightGray: "#ffffffb2",
   black: "#00010D",
+  lightBlack: "#2f3030",
 };
 
-export { white, beige, blue, gray, lightGray, black };
+export {
+  white,
+  beige,
+  red,
+  yellow,
+  blue,
+  lightBlue,
+  gray,
+  lightGray,
+  black,
+  lightBlack,
+};
