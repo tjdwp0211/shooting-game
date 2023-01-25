0e6cdee632f2bc4be3a3a58a5480d9b28b5ef564
tjdwp0211 <rlatjdwp0211@naver.com>
2023-01-25 15:15:00 +0900

    style: score modal #19
    - Create Score modal Container

--- src/components/check-score/Presenter.tsx
+++ src/components/check-score/Presenter.tsx
@@ -1,18 +1,41 @@
-import styled from "@emotion/styled";
 import React from "react";
-import Box from "../box/Box";
+import styled from "@emotion/styled";
+import Overlay from "../overlay/Overlay";
+import { lightBlack } from "../../style/palette/palette";
 
 function Presenter() {
   return (
-    <Wrapper>
-      <Box>Check Your Score</Box>
-    </Wrapper>
+    <>
+      <Overlay />
+      <Wrapper>
+        <Container></Container>
+      </Wrapper>
+    </>
   );
 }
 
 export default Presenter;
 
 const Wrapper = styled.article`
-  width: 80vw;
-  height: 50vh;
+  width: 70vw;
+  height: 70vh;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 6px;
+  z-index: 1;
+  box-shadow: 0px 0px 12px 0px ${lightBlack};
+  @media (max-width: 768px) {
+    width: 80vw;
+    height: 60vh;
+    transform: translate(-50%, -45%);
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  border-radius: 6px;
 `;
