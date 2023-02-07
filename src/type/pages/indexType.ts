7337781f445ea84b0e9f22ba6f34351bde8ce13d
김성제 <rlatjdwp0211@naver.com>
2023-02-07 18:47:54 +0900

    refactor: Index page Presenter Type #25
    - Add PresenterProps in indexType

--- src/type/pages/indexType.ts
+++ src/type/pages/indexType.ts
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Dispatch } from "react";
+import { ChartDatas } from "../components/chartType";
 
 interface GameProgress {
   start: boolean;
   checkScore: boolean;
 }
 
-export type { GameProgress };
+interface PresenterProps {
+  gameProgress: GameProgress;
+  setGameProgress: Dispatch<React.SetStateAction<GameProgress>>;
+  handleStackingHit: (e: React.MouseEvent) => void;
+  dataForChart: ChartDatas[];
+}
+
+export type { GameProgress, PresenterProps };
