13011e96a0dcf3f6811b025895bd4e0f1ee51954
김성제 <rlatjdwp0211@naver.com>
2023-02-08 10:41:13 +0000

    refactor: indexpage types #25
    - Apply type refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { pullTrigger } from "../redux/root";
 import { GameProgress } from "../type/pages/indexType";
 import Presenter from "../layout/pages/index/Presenter";
+import useCheckStorageItems from "../customhook/useCheckStorageItems";
+import { UseCheckStorageItems } from "../type/customhook/useCheckStorageItemsType";
 
 function Home() {
   const dispatch = useDispatch();
@@ -10,19 +13,24 @@ function Home() {
     start: false,
     checkScore: false,
   });
+  const [storageItems, setStorageItems] = useState<UseCheckStorageItems>();
+  useEffect(() => {
+    setStorageItems(useCheckStorageItems());
+  }, []);
 
   const dataForChart = [
     {
-      type: "line" as "line",
+      type: "bar" as "line",
       label: "Hit",
-      data: [12, 12, 21, 123, 124, 3],
+      data: storageItems && storageItems.makeHit,
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgb(255, 99, 132)",
     },
     {
       type: "bar" as "line",
       label: "Time To Clear",
-      data: [12, 12, 21, 123, 124, 3],
+      data: storageItems && storageItems.time,
+      borderColor: "rgb(75, 192, 192)",
       backgroundColor: "rgb(75, 192, 192)",
     },
   ];
