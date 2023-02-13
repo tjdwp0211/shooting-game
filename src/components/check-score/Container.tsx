5dfc649e5698dc64ff90fd8d11142a612056780b
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-13 17:31:16 +0900

    feat: Add playTime key #26
    - When game is finished, set item in local storage with playTimes

--- src/components/check-score/Container.tsx
+++ src/components/check-score/Container.tsx
@@ -6,22 +6,32 @@ import { ContainerProps } from "../../type/components/checkScoreType";
 
 function Container({ setGameProgress }: ContainerProps) {
   const dispatch = useDispatch();
-  const { time, stackingHit } = useSelector((state: Store) => state.gameState);
+  const { timeToClear, stackingHit } = useSelector(
+    (state: Store) => state.gameState
+  );
   const localStorageItems = JSON.parse(localStorage.getItem("dashboard")) || [];
 
+  const date = new Date();
+  const playTimes = `${date.getDate()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
   useEffect(() => {
-    const { length } = stackingHit.filter((el) => el);
+    const { length } = stackingHit.filter(el => el);
     const newItem = [
       ...localStorageItems,
-      { time: time, stackingHit: stackingHit, makeHit: length },
+      {
+        playTimes: playTimes,
+        timeToClear: timeToClear,
+        stackingHit: stackingHit,
+        makeHit: length,
+      },
     ];
     return () => localStorage.setItem("dashboard", JSON.stringify(newItem));
-  });
+  }, []);
 
   const resetGameState = (e: React.MouseEvent) => {
     e.stopPropagation();
     dispatch(clearGameState());
-    setGameProgress((prev) => ({ ...prev, start: false, checkScore: false }));
+    setGameProgress(prev => ({ ...prev, start: false, checkScore: false }));
   };
 
   return <Presenter resetGameState={resetGameState} />;
