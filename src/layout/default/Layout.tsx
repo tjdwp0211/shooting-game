ef6dd3d198074a18bc27a1aead0c43d7c7bb74c6
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-20 16:36:20 +0900

    feat: Apply check device hook #17
    - Apply useCheckDeviceSize in global Layout component

--- src/layout/default/Layout.tsx
+++ src/layout/default/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { useCheckDeviceSize } from "../../customhook";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,6 +9,7 @@ interface LayoutProps {
 }
 
 function Layout({ children, indexPage }: LayoutProps) {
+  useCheckDeviceSize();
   return (
     <>
       <Header />
