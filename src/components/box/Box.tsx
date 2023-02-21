e7029238405781aaab3a9571712402f78590ce08
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-21 14:36:18 +0900

    style: Box Component #27
    - Delete dark mode style code in Box Component

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { lightGray, black } from "../../style/palette/palette";
+import { black } from "../../style/palette/palette";
 
 interface BoxProps {
   children: React.ReactNode;
@@ -8,21 +8,17 @@ interface BoxProps {
 }
 
 function Box({ children, bgColor }: BoxProps) {
-  return <Container bgColor={bgColor}>{children}</Container>;
+  return <BoxContainer bgColor={bgColor}>{children}</BoxContainer>;
 }
 
 export default Box;
 
-const Container = styled.div<{ bgColor: string }>`
+const BoxContainer = styled.div<{ bgColor: string }>`
   width: 100%;
   height: 100%;
   background-color: ${props => props.bgColor};
   color: ${black};
   display: flex;
-  @media (prefers-color-scheme: dark) {
-    background-color: ${lightGray};
-    color: white;
-  }
   @media (width < 642px) {
     flex-direction: column;
   }
