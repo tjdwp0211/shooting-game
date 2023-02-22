4add55be20d3343a671131536dccfca4c58808ff
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-22 17:25:38 +0900

    refactor: useCheckDeviceSize #31
    - Modify custom hook

--- src/customhook/useCheckDeviceSize.tsx
+++ src/customhook/useCheckDeviceSize.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { resizing } from "../redux/root";
+import { getDeviceSize } from "../redux/root";
 
 function useCheckDeviceSize() {
   const dispatch = useDispatch();
   const changeDeviceSize = () => {
     dispatch(
-      resizing(
-        (() => {
-          const { innerWidth: width } = window;
-          if (width < 768) return 8;
-          if (width < 1024) return 10;
-          else return 12;
-        })()
+      getDeviceSize(
+        (() => ({
+          deviceWidth: window.innerWidth,
+          deviceHeight: window.innerHeight,
+        }))()
       )
     );
   };
