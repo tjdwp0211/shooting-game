1b9b752d350145addbb0525f41d1e0080f61d56e
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-23 18:51:55 +0900

    refactor: Export SelectBox #27
    - SelectBox is export in components/index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import CheckScore from "./check-score/Container";
 import Chart from "./chart/Chart";
 import Box from "./box/Box";
 import Text from "./text/Text";
+import SelectBox from "./selectBox/SelectBox";
 
-export { TargetBoard, CurGameState, CheckScore, Chart, Box, Text };
+export { TargetBoard, CurGameState, CheckScore, Chart, Box, Text, SelectBox };
