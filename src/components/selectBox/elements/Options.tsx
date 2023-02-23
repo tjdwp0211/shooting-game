1c4ad14ff4ef97d440b279db213a351d5e8d98b4
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-23 18:57:44 +0900

    style: Options #27
    - Create Options Component that elements of select box

---
+++ src/components/selectBox/elements/Options.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Text from "../../text/Text";
+import { regularFont } from "../../../style/fonts/inedx";
+import { gray } from "../../../style/palette/palette";
+
+interface OptionsProps {
+  view: boolean;
+  options: string[];
+  mainColor: string;
+}
+
+function Options({ view, options, mainColor }: OptionsProps) {
+  const createOptionElements = () => {
+    return options.map((option, i) => (
+      <Text size={24} weight={regularFont} key={i}>
+        {option}
+      </Text>
+    ));
+  };
+
+  return (
+    <OptionsWrapper view={view} mainColor={mainColor}>
+      {createOptionElements()}
+    </OptionsWrapper>
+  );
+}
+
+export default Options;
+
+const OptionsWrapper = styled.div<{ view: boolean; mainColor: string }>`
+  width: 90%;
+  height: ${props => (props.view ? 180 : 0)}%;
+  border-radius: 12px 0 12px 12px;
+  background-color: ${props => props.mainColor};
+  color: ${props => (props.view ? "white" : props.mainColor)};
+  box-shadow: 0px 0px 4px 0px ${props => props.view && gray};
+  transition-property: height, color;
+  transition-duration: 0.5s, 0.3s;
+  overflow: hidden;
+  z-index: 2;
+  cursor: pointer;
+  p {
+    width: 100%;
+    height: 33%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  @media (max-width: 640px) {
+    display: flex;
+    align-items: center;
+    height: ${props => (props.view ? 100 : 0)}%;
+    border-radius: 12px 0 0 12px;
+  }
+`;
