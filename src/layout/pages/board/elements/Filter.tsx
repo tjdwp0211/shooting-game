dcafe59530eb29fa835a5751b1eab8b8602cafc2
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-23 18:54:27 +0900

    feat: SelectBox view control #27
    - View of SelectBox is managed in Filter

--- src/layout/pages/board/elements/Filter.tsx
+++ src/layout/pages/board/elements/Filter.tsx
@@ -1,19 +1,91 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "@emotion/styled";
+import { SelectBox } from "../../../../components";
+import { green, lightBlue, pink } from "../../../../style/palette/palette";
 
 function Filter() {
+  const [{ timeSelect, scoreSelect, hitsSelect }, setSelectBoxView] = useState({
+    timeSelect: false,
+    scoreSelect: false,
+    hitsSelect: false,
+  });
+
+  const handleOptionsView = useCallback((key: string) => {
+    const closeAllSelectView = {
+      timeSelect: false,
+      scoreSelect: false,
+      hitsSelect: false,
+    };
+    setSelectBoxView(prev => ({ ...closeAllSelectView, [key]: !prev[key] }));
+  }, []);
+
+  const {
+    handleTimeOptionsView,
+    handleScoreOptionsView,
+    handleHitsOptionsView,
+  } = {
+    handleTimeOptionsView() {
+      handleOptionsView("timeSelect");
+    },
+    handleScoreOptionsView() {
+      handleOptionsView("scoreSelect");
+    },
+    handleHitsOptionsView() {
+      handleOptionsView("hitsSelect");
+    },
+  };
+
   return (
-    <FilterWrapper>
-      <FilterWrapper>제일 긴 시간</FilterWrapper>
-      <FilterWrapper>제일 짧은 시간</FilterWrapper>
-      <FilterWrapper>제일 높은 점수</FilterWrapper>
-      <FilterWrapper>제일 낮은 점수</FilterWrapper>
-    </FilterWrapper>
+    <SelectBoxWrapper>
+      <input />
+      <SelectBox
+        view={timeSelect}
+        mainColor={lightBlue}
+        handleOnClick={handleTimeOptionsView}
+        options={["All", "Quick", "Slow"]}
+      >
+        Time To Clear
+      </SelectBox>
+      <SelectBox
+        view={scoreSelect}
+        mainColor={green}
+        handleOnClick={handleScoreOptionsView}
+        options={["All", "High", "Low"]}
+      >
+        Score
+      </SelectBox>
+      <SelectBox
+        view={hitsSelect}
+        mainColor={pink}
+        handleOnClick={handleHitsOptionsView}
+        options={["All", "High", "Low"]}
+      >
+        Hits
+      </SelectBox>
+    </SelectBoxWrapper>
   );
 }
 
 export default Filter;
 
-const FilterWrapper = styled.div`
-  background-color: black;
+const SelectBoxWrapper = styled.article`
+  width: 100%;
+  height: 80%;
+  padding: 24px 0px;
+  display: grid;
+  grid-template-rows: 20px 20% 20% 20%;
+  align-items: center;
+  justify-items: center;
+  gap: 2vw;
+
+  @media (width < 1024px) {
+    align-content: center;
+    @media (width < 640px) {
+      align-content: start;
+      padding: 0;
+      & > div {
+        width: 50%;
+      }
+    }
+  }
 `;
