5093047decaa58584284a89de13985a90e0f3747
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-23 18:56:55 +0900

    style: ArrowButton #27
    - Create ArrowButton for open select box

---
+++ src/components/selectBox/elements/ArrowButton.tsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+interface ArrowButtonProps {
+  view: boolean;
+  mainColor: string;
+  handleOnClick: () => void;
+}
+
+function ArrowButton({ view, mainColor, handleOnClick }: ArrowButtonProps) {
+  return (
+    <ArrowImgWrapper view={view} mainColor={mainColor} onClick={handleOnClick}>
+      <Arrow view={view} mainColor={mainColor} />
+    </ArrowImgWrapper>
+  );
+}
+
+export default ArrowButton;
+
+const ArrowImgWrapper = styled.div<{ view: boolean; mainColor: string }>`
+  width: 10%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  color: black;
+  background-color: ${props => (props.view ? "white" : props.mainColor)};
+  transition: background-color 0.5s;
+  border-radius: 0 12px 12px 0;
+`;
+
+const Arrow = styled.div<{ view: boolean; mainColor: string }>`
+  width: 8px;
+  height: 8px;
+  border: 1px solid ${props => (props.view ? props.mainColor : "white")};
+  border-bottom: none;
+  border-left: none;
+  transform: rotate(${props => (props.view ? -45 : 135)}deg);
+  transition: transform 0.3s;
+  margin-bottom: ${props => (props.view ? -4 : 4)}px;
+`;
