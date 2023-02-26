4de704056459a290b9d5e3afdf8643e025cb1aab
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-26 23:34:44 +0900

    style: SelectBox Placeholde #34
    - Add placeholder in SelectBox Component

--- src/components/selectBox/SelectBox.tsx
+++ src/components/selectBox/SelectBox.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { ArrowButton, Options } from "./elements";
-import { gray } from "../../style/palette/palette";
+import { black, gray } from "../../style/palette/palette";
 import { SelectBoxProps } from "../../type/components/selectBoxTypes";
+import Text from "../text/Text";
+import { regularFont } from "../../style/fonts/inedx";
 
 function SelectBox(props: SelectBoxProps) {
   const {
@@ -14,7 +16,12 @@ function SelectBox(props: SelectBoxProps) {
   } = props;
   return (
     <SelectBoxWrapper view={view}>
-      <Options options={options} view={view} mainColor={mainColor} />
+      <Text size={16} weight={regularFont}>
+        {placeholder}
+      </Text>
+      <OptionsWrapper view={view}>
+        <Options options={options} view={view} mainColor={mainColor} />
+      </OptionsWrapper>
       <ArrowButton
         view={view}
         mainColor={mainColor}
@@ -34,14 +41,34 @@ const SelectBoxWrapper = styled.div<{ view: boolean }>`
   background-color: white;
   box-shadow: 0px 0px 4px 0px ${props => !props.view && gray};
   position: relative;
-  border-radius: 12px;
+  border-radius: ${props => (props.view ? "12px 12px 12px 0px" : "12px")};
   & > p {
     width: 80%;
     height: 100%;
-    color: black;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-left: 10%;
+    color: ${black};
   }
 
   @media (max-width: 820px) {
     height: 50%;
+    & > p {
+      width: 80%;
+    }
+    @media (width < 640px) {
+      padding: 0;
+      & > p {
+        padding-top: 4px;
+      }
+    }
   }
+`;
+
+const OptionsWrapper = styled.div<{ view: boolean }>`
+  width: 100%;
+  height: 100%;
+  position: absolute;
 `;
