747cd72d6fa457f4d28bc32183911b7deaecf0f4
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-26 23:28:03 +0900

    feat: Create filtering functions #34
    - Create filtering functions in pages/board

--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Presenter from "../layout/pages/board/Presenter";
 import { useCheckStorageItems } from "../customhook";
-import { UseCheckStorageItems } from "../type/customhook/useCheckStorageItemsType";
+import {
+  StorageItems,
+  SortStorageItemsByKey,
+  UseCheckStorageItems,
+} from "../type/customhook/useCheckStorageItemsType";
 
 function Board() {
   const [storageItems, setStorageItems] = useState<UseCheckStorageItems>();
@@ -9,8 +13,42 @@ function Board() {
     setStorageItems(useCheckStorageItems);
   }, []);
 
+  const newAllTries = (arr: StorageItems[]) => {
+    return {
+      timeToClear: arr.map(el => el.timeToClear),
+      makeHit: arr.map(el => el.makeHit),
+      stackingScore: arr.map(el => el.stackingScore),
+      playerNames: arr.map(el => el.playerName),
+    };
+  };
+
+  const increasingSorting = (key: keyof SortStorageItemsByKey[]) => {
+    const { getItems } = storageItems;
+    const sortingArray = [...getItems].sort((a, b) => a[key] - b[key]);
+    setStorageItems(prev => ({ ...prev, allTries: newAllTries(sortingArray) }));
+  };
+
+  const descendingSorting = (key: keyof SortStorageItemsByKey[]) => {
+    const { getItems } = storageItems;
+    const sortingArray = [...getItems].sort((a, b) => b[key] - a[key]);
+    setStorageItems(prev => ({ ...prev, allTries: newAllTries(sortingArray) }));
+  };
+
+  const drawByPlayerName = (input: string) => {
+    const { getItems } = storageItems;
+    const filteringArr = getItems
+      .map(item => item.playerName === input && item)
+      .filter(elementIsDefined => elementIsDefined);
+
+    setStorageItems(prev => ({ ...prev, allTries: newAllTries(filteringArr) }));
+  };
+
+  const defaultSorting = () => {
+    setStorageItems(useCheckStorageItems);
+  };
+
   const allTriesChartsProps = {
-    playerNames: storageItems ? storageItems.recentlyTrys.playerNames : [],
+    playerNames: storageItems ? storageItems.allTries.playerNames : [],
     dataForChart: [
       {
         type: "bar" as "line",
@@ -36,7 +74,16 @@ function Board() {
     ],
   };
 
-  return <Presenter allTriesChartsProps={allTriesChartsProps} />;
+  return (
+    <Presenter
+      storageItems={storageItems}
+      allTriesChartsProps={allTriesChartsProps}
+      increasingSorting={increasingSorting}
+      descendingSorting={descendingSorting}
+      drawByPlayerName={drawByPlayerName}
+      defaultSorting={defaultSorting}
+    />
+  );
 }
 
 export default Board;
