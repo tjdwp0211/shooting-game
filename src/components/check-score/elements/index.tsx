f51e3bc050f61e8c9abee57449068484dfafa068
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-27 16:15:01 +0900

    refactor: Moved in Component folder #29
    - Apply refactoring

--- src/components/check-score/elements/index.tsx
+++ src/components/check-score/elements/index.tsx
@@ -1,5 +1,4 @@
 import HitScore from "./HitScore";
 import Time from "./TimeToClear";
-import PlayerNameInput from "./PlayerNameInput";
 
-export { HitScore, Time, PlayerNameInput };
+export { HitScore, Time };
