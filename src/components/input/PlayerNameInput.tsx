33ee3972692d45c97e8307ae780fae3ac77bd543
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-02 17:29:03 +0900

    feat: input focus handle #35
    - Input focus handle when user using mobile

--- src/components/input/PlayerNameInput.tsx
+++ src/components/input/PlayerNameInput.tsx
@@ -22,9 +22,11 @@ const StyledInput = styled.input<{ blocking: boolean; mainColor: string }>`
   width: 204px;
   height: 32px;
   border: none;
+  outline: none;
   appearance: none;
   -moz-appearance: none;
   -webkit-appearance: none;
+  -webkit-border-radius: none;
   border-bottom-left-radius: 6px;
   border-left: 2px solid ${props => (props.blocking ? yellow : props.mainColor)};
   border-bottom: 2px solid
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,10 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=no"
+          />
           <meta name="description" content="Test your aiming" />
         </Head>
         <body>
