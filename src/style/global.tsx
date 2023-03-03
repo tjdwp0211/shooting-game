7954a1719ae6ee0dfd4d00d92ee456b4b32cabae
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-03 18:42:54 +0900

    refactor: Delete #35
    - Delete unusable code

--- src/style/global.tsx
+++ src/style/global.tsx
@@ -4,9 +4,6 @@ import { regularFont } from "./fonts/inedx";
 import { black, green, white } from "./palette/palette";
 
 const globalStyle = css`
-  /* #__next {
-    overflow: hidden;
-  } */
   * {
     box-sizing: border-box;
   }
