8428a2b8d8c4614ec4c04af1e0f64204b9740561
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-03 20:10:14 +0900

    refactor: #35
    - Function refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { pullTrigger } from "../redux/root";
 import { GameProgress } from "../type/pages/indexType";
@@ -12,12 +12,15 @@ function Home() {
   });
   const { start, checkScore } = gameProgress;
 
-  const handleStackingScore = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    if (!start && !checkScore)
-      setGameProgress(prev => ({ ...prev, start: !prev.start }));
-    else if (start) dispatch(pullTrigger(Number(e.currentTarget.id)));
-  };
+  const handleStackingScore = useCallback(
+    (e: React.MouseEvent) => {
+      e.stopPropagation();
+      if (!start && !checkScore)
+        setGameProgress(prev => ({ ...prev, start: !prev.start }));
+      else if (start) dispatch(pullTrigger(Number(e.currentTarget.id)));
+    },
+    [start, checkScore, dispatch]
+  );
 
   return (
     <Presenter
