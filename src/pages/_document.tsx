078c43caf17a3fd760a1a589069195dae5625269
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-06 16:47:06 +0900

    build: google site verification #35
    - Apply in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,12 +17,16 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0, user-scalable=no"
+          />
+          <meta
+            name="google-site-verification"
+            content="5wAsgsnNWjftOr94sJMP5Iso_9nvPPZvnXUCFnZv_-0"
           />
           <meta name="description" content="Test your aiming" />
         </Head>
