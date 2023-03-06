19e4dcf4d5a12ef98b212b05562b2e28cb9d9c91
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-06 18:33:08 +0900

    style: Timer #35
    - Modify timer component

--- src/components/check-score/elements/TimeToClear.tsx
+++ src/components/check-score/elements/TimeToClear.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import React from "react";
 import { useSelector } from "react-redux";
 import { Store } from "../../../redux/root";
-import { boldFont, regularFont } from "../../../style/fonts/inedx";
+import { boldFont, lightFont, regularFont } from "../../../style/fonts/inedx";
 import Text from "../../text/Text";
 
 function Time() {
@@ -10,10 +10,13 @@ function Time() {
 
   return (
     <TimeContainer>
-      <Text size={32} weight={boldFont}>
-        Time to clear : {timeToClear}
-      </Text>
       <Text size={20} weight={regularFont}>
+        Time to clear :
+      </Text>
+      <Text size={32} weight={boldFont}>
+        {timeToClear}
+      </Text>
+      <Text size={20} weight={lightFont}>
         s
       </Text>
     </TimeContainer>
@@ -24,8 +27,13 @@ export default Time;
 
 const TimeContainer = styled.div`
   display: flex;
-  align-items: center;
-  p:nth-last-of-type(1) {
-    padding: 8px 0px 0px 2px;
+  align-items: flex-end;
+  gap: 2px;
+
+  p:nth-of-type(1) {
+    padding: 0px 4px 4px 0px;
+  }
+  p:nth-of-type(3) {
+    padding: 0px 0px 2px 4px;
   }
 `;
