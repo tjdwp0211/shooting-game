0d09c9fe2b1dc662c3426b4cb29f22e4e8f24e47
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-07 12:44:40 +0900

    build: SEO #35
    - Add meta tags in _app.tsx for SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,15 @@ function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, user-scalable=no"
         />
-        <meta name="description" content="Test your aim" />
+        <meta property="og:title" content="Test your aim" />
+        <meta
+          property="og:title"
+          name="description"
+          content="If you click center of target, you can get more score! Test your aim and check your score for Free."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://aimingtrainer.com/" />
+        <meta property="og:article:author" content="Aim Trainer Game" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Global styles={globalStyle} />
