c64d0fb1ee4506804e431119b0c574398281db81
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-10 20:01:00 +0900

    feat: game state reducer #36
    - Create score range logic in reducer

--- src/redux/gameState/reducer.tsx
+++ src/redux/gameState/reducer.tsx
@@ -16,9 +16,23 @@ const gameStatus = createSlice({
   name: "gameStatus",
   initialState: initialState,
   reducers: {
-    pullTrigger(state, action: PayloadAction<number>) {
+    pullTrigger(state, action: PayloadAction<{ x: number; y: number } | null>) {
       state.remainBullets = state.remainBullets - 1;
-      state.stackingScore.push(action.payload);
+
+      const { x: calcedX, y: calcedY } = action.payload;
+      const validateTarget = calcedX > calcedY ? calcedX : calcedY;
+
+      if (calcedX === null && calcedY === null) state.stackingScore.push(0);
+      else if (0 <= validateTarget && validateTarget <= 5)
+        state.stackingScore.push(10);
+      else if (6 <= validateTarget && validateTarget <= 13)
+        state.stackingScore.push(8);
+      else if (14 <= validateTarget && validateTarget <= 24)
+        state.stackingScore.push(6);
+      else if (25 <= validateTarget && validateTarget <= 35)
+        state.stackingScore.push(4);
+      else if (36 <= validateTarget && validateTarget <= 44)
+        state.stackingScore.push(2);
     },
     clearGameState(state) {
       state.remainBullets = 30;
