2b11061494e9a451e725e5a841ae83fa53fa8d16
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-10 19:19:30 +0900

    feat: Delete handleStackingScore function
    - Delete function and validate moved in children component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,15 @@
-import React, { useCallback, useState } from "react";
-import { useDispatch } from "react-redux";
-import { pullTrigger } from "../redux/root";
+import React, { useState } from "react";
 import { GameProgress } from "../type/pages/indexType";
 import Presenter from "../layout/pages/index/Presenter";
 
 function Home() {
-  const dispatch = useDispatch();
   const [gameProgress, setGameProgress] = useState<GameProgress>({
     start: false,
     checkScore: false,
   });
-  const { start, checkScore } = gameProgress;
-
-  const handleStackingScore = useCallback(
-    (e: React.MouseEvent<HTMLDivElement>) => {
-      e.stopPropagation();
-      if (!start && !checkScore)
-        setGameProgress(prev => ({ ...prev, start: !prev.start }));
-      else if (start)
-        dispatch(pullTrigger(Number((e.target as HTMLDivElement).id)));
-    },
-    [start, checkScore, dispatch]
-  );
 
   return (
-    <Presenter
-      gameProgress={gameProgress}
-      setGameProgress={setGameProgress}
-      handleStackingScore={handleStackingScore}
-    />
+    <Presenter gameProgress={gameProgress} setGameProgress={setGameProgress} />
   );
 }
 
