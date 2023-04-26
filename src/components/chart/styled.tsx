fe490fe8f34b484cd8317dc7ae38cfe381df926d
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-26 13:39:49 +0900

    refactor: refactoring #40
    - refactoring

---
+++ src/components/chart/styled.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.article`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius: 6px;
+  padding: 12px 0px;
+`;
