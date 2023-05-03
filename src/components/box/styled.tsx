38fc50b5691236fac4cd7bd77b9b6b708e7a4d09
Sung Je Kim <rlatjdwp0211@naver.com>
2023-05-03 19:51:46 +0900

    refactor: refactoring #40
    - refactoring

---
+++ src/components/box/styled.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "@emotion/styled";
+import * as PALETTE from "../../style/palette/palette";
+
+export const BoxContainer = styled.div<{ bgColor: string }>`
+  width: 100%;
+  height: 100%;
+  background-color: ${props => props.bgColor};
+  color: ${PALETTE.black};
+  display: flex;
+  display: -webkit-flex;
+  align-items: center;
+  justify-content: space-around;
+  -webkit-align-items: center;
+  -webkit-justify-content: space-around;
+  gap: 12px;
+  @media only screen and (max-width: 768px) {
+    @media (max-height: 375px) or (orientation: landscape) {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+    }
+    flex-direction: column;
+    align-items: center;
+    -webkit-flex-direction: column;
+    -webkit-align-items: center;
+  }
+`;
