6884ca1edf5eb7bf441e7ede0876d4f33ead7425
Sung Je Kim <rlatjdwp0211@naver.com>
2023-05-07 16:30:22 +0000

    refactor: refactoring #40
    - refactoring

---
+++ src/components/TimeToClear/styled.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export const TimeContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 2px;
+
+  p:nth-of-type(1) {
+    padding: 0px 4px 4px 0px;
+  }
+  p:nth-of-type(3) {
+    padding: 0px 0px 2px 4px;
+  }
+`;
