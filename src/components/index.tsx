ece52949dcd2e916a0c0af25a258f3418d829d29
Sung Je Kim <rlatjdwp0211@naver.com>
2023-05-31 16:30:54 +0900

    refactor: refactoring #40
    - refactoring

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,11 @@
 import TargetBoard from "./target-board/Container";
-import CurGameState from "./cur-game-info";
 import CheckScore from "./check-score/Container";
 import Chart from "./chart";
 import Box from "./box";
 import Text from "./text/Text";
 import SelectBox from "./selectBox/SelectBox";
 import Filter from "./filter/Container";
+import CurGameState from "./cur-game-info";
 
 export {
   TargetBoard,
